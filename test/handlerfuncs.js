3436395ef0d40a4fbca20256b9a86dbc95607293
Eugenia Morales <eumorale@calpoly.edu>
2022-06-07 12:47:36 -0700

    Revert "unused folder"
    This reverts commit 5d9b16ba0de45c3bc5fca38788b83e39b598f9e4.

---
+++ test/handlerfuncs.js
@@ -0,0 +1,37 @@
+const bodyParser = require("body-parser");
+const routesHandler = require("./routes/handler.js");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const express = require("express");
+const router = express.Router();
+const Schemas = require("../models/Schemas.js");
+
+let dbConnection;
+
+function getDBconnection() {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB Connected!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+async function getIngredients(ingredient) {
+  const ingredients = Schemas.Ingredients;
+  let result;
+
+  if (ingredient === undefined) {
+    result = await ingredients.find();
+  } else if (ingredient) {
+    result = await ingredients.find(ingredient);
+  }
+
+  return result;
+}
